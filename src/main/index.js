83d8cca1daccb4ae0a64ce2488317871479a8e37
pengtao <pengtao@newrank.cn>
2019-09-25 17:39:11 +0800

    项目初始化

---
+++ src/main/index.js
@@ -0,0 +1,72 @@
+'use strict'
+
+import { app, BrowserWindow, Menu } from 'electron'
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+const winURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:7777`
+  : `file://${__dirname}/index.html`
+
+function createWindow () {
+
+  /* 隐藏electron创听的菜单栏 */
+  Menu.setApplicationMenu(null)
+
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 600,
+    useContentSize: true,
+    width: 1000,
+    backgroundColor: '#2e2c29'
+  })
+
+  mainWindow.loadURL(winURL)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
